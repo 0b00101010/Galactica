f85eb1ad5f32c5d8d6a6900f4492c576507f4be3
Komorio <twilivez@gmail.com>
2020-07-02 09:43:13 +0900

    [ADD] PlayerCharacterController 추가
    00. 공이 공전하는 시스템

---
+++ Assets/01.Scripts/PlayerCharacter/PlayerCharacterController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacterController : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private GameObject redObject;
+
+    [SerializeField]
+    private GameObject blueObject;
+
+    [Header("Values")]
+    [SerializeField]
+    private float rotateSpeed;
+
+    [SerializeField]
+    private int radiusValue;
+
+    private GameObject mainObject;
+    private GameObject subObject;
+
+    private void Awake(){
+        mainObject = redObject;
+        subObject = blueObject;
+    }
+
+    private void Update(){
+        RotateSubObject();
+
+        if(Input.GetKeyDown(KeyCode.A)){
+            ChangeObject();
+        }
+    }
+
+    private void RotateSubObject(){
+        subObject.transform.RotateAround(mainObject.transform.Position(), Vector3.forward, radiusValue + (rotateSpeed));
+    }
+
+    private void ChangeObject(){
+        GameObject temp = mainObject;
+
+        mainObject = subObject;
+        subObject = temp;
+    }
+}
