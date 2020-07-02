a62f9e7d2a08a7cdecf8e16a47c2714cff9e3fd6
Komorio <twilivez@gmail.com>
2020-07-02 10:44:44 +0900

    [ADD] TileObject 추가
    00. Execute 시 한번 점멸함

---
+++ Assets/01.Scripts/TileObject/TileObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class TileObject : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    private Color defaultColor;
+
+    private Tween blinkTween;
+    private IEnumerator blinkCoroutine;
+    
+    private float fadeDuration;
+
+    private void Awake(){
+        spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+        defaultColor = spriteRenderer.color;
+
+        fadeDuration = 0.5f;
+    }
+
+    public void Execute(){
+        blinkCoroutine?.Stop(this);
+        blinkCoroutine = BlinkCoroutine().Start(this);
+    }
+
+    private IEnumerator BlinkCoroutine(){
+        spriteRenderer.color = defaultColor;
+        blinkTween?.Kill();
+
+        blinkTween = spriteRenderer.DOColor(Color.white, fadeDuration);
+        yield return blinkTween.WaitForCompletion();
+
+        blinkTween = spriteRenderer.DOColor(defaultColor, fadeDuration);
+        yield return blinkTween.WaitForCompletion();
+
+        blinkTween = null;
+    }
+}
