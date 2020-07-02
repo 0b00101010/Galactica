3ad8bcfb202faae8606943a75046c4910a752b46
Komorio <twilivez@gmail.com>
2020-07-02 11:41:33 +0900

    [ADD] MapPhaser 추가
    00. csv 형식 Text 파일을 읽어 맵을 생성함

---
+++ Assets/01.Scripts/InGameManager/MapPhaser.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MapPhaser : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private GameObject tileObject;
+
+    [SerializeField]
+    private GameObject wallObject;
+
+    [SerializeField]
+    private GameObject playerCharacter;
+
+    [SerializeField]
+    private GameObject portalObject;
+
+    public void MapCreate(string mapName = "Test"){
+        string mapText = Resources.Load<TextAsset>("/MapFile/" + mapName).text;
+        string[] mapTextColumn = mapText.Split('\n');
+
+        Vector2 objectGeneratePosition = Vector2.zero;
+        
+        for(int i = 0; i < mapTextColumn.Length; i++){
+            string[] mapTextRow = mapTextColumn[i].Split(',');
+            for(int j = 0; j < mapTextRow.Length; j++){
+                int objectIndex = int.Parse(mapTextRow[j]);
+                GameObject generateObject = null;
+
+                switch(objectIndex){
+                    case 0:
+                    generateObject = tileObject;
+                    break;
+
+                    case 1:
+                    generateObject = wallObject;
+                    break;
+
+                    case 2:
+                    generateObject = playerCharacter;
+                    break;
+
+                    case 3:
+                    generateObject = portalObject;
+                    break;
+
+                }
+
+                objectGeneratePosition.x = j * 1.5f;
+                objectGeneratePosition.y = i * 1.5f;
+
+                Instantiate(generateObject, objectGeneratePosition, Quaternion.identity);
+            }
+        }
+    }
+
+}
--- README.md
+++ README.md
@@ -6,3 +6,13 @@ Design : 정용우([@Reuse1020](https://github.com/Reuse1020)) <br>
 
 ## Editor
 UnityVersion : 2020.1.0b12
+
+# Map File
+* MapFile은 csv 형식 text파일을 사용합니다.
+
+```
+0 : Tile Object
+1 : Wall Object
+2 : Player Character Object
+3 : PortalObject
+```
