e9475b25ea8ba19129fd2c3d39a410a679989dd3
Komorio <twilivez@gmail.com>
2020-07-06 12:37:55 +0900

    [UPDATE] Timer 기능 추가

--- Assets/01.Scripts/InGameManager/InGameManager.cs
+++ Assets/01.Scripts/InGameManager/InGameManager.cs
@@ -5,9 +5,16 @@ using UnityEngine;
 public class InGameManager : MonoBehaviour
 {
     private MapPhaser mapPhaser;
+    private UIController uiController;
 
     private void Awake(){
+        uiController = gameObject.GetComponent<UIController>();
         mapPhaser = gameObject.GetComponent<MapPhaser>();
+
         mapPhaser.MapCreate();
+    }
+
+    public void Death(){
+
     }
 }
---
+++ Assets/01.Scripts/InGameManager/UIController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIController : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private Image timerImage;
+
+    [SerializeField]
+    private Text timerText;
+
+    [Header("Values")]
+    [SerializeField]
+    private float defaultTime;
+    private float usedTime;
+ 
+    [Header("Events")]
+    [SerializeField]
+    private VoidEvent deathEvent;
+
+    public void SetTimer(float value){
+        defaultTime = value;
+    }
+
+    private void Update(){
+        float remainTime = defaultTime - usedTime;
+
+        if(!(remainTime > 0.0f)){
+            deathEvent.Invoke();
+            this.enabled = false;
+
+        }
+
+        usedTime += Time.deltaTime;
+        
+        timerText.text = remainTime.ToString("F0");
+        timerImage.fillAmount = 1 - (usedTime / defaultTime);
+    }
+}
