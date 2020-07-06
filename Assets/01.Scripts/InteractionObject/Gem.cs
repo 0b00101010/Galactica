705fff9b83c04ca9af17cad1a92289a042990b6f
Komorio <twilivez@gmail.com>
2020-07-06 13:47:55 +0900

    [ADD] Gem Object 추가

---
+++ Assets/01.Scripts/InteractionObject/Gem.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Gem : InteractionObject
+{
+    private Tween interactionTween;
+    private bool isInteraction;
+    
+    public override void Interaction(){
+        if(!isInteraction){
+            isInteraction = true;
+            InteractionCoroutine().Start(this);
+        }
+    }
+
+    private IEnumerator InteractionCoroutine(){
+        interactionTween = gameObject.transform.DOScale(1.5f, 1.0f);
+        yield return interactionTween.WaitForCompletion();
+
+        gameObject.SetActive(false);
+    }
+    
+}
