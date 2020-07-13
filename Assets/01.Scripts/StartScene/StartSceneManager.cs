4be1e506f708b535a27635c362b9ab53dae764cd
Komorio <twilivez@gmail.com>
2020-07-13 15:05:28 +0900

    [UPDATE] Stage이동에 자연스러움 추가

--- Assets/01.Scripts/StartScene/StartSceneManager.cs
+++ Assets/01.Scripts/StartScene/StartSceneManager.cs
@@ -2,16 +2,22 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+using DG.Tweening;
 
 public class StartSceneManager : MonoBehaviour
 {
     public static StartSceneManager instance;
     
+    [Header("Objects")]
     [SerializeField]
     private UIWidget titlePanel;
 
     [SerializeField]
     private UIWidget stagePanel;
+
+    [SerializeField]
+    private Image blackBackground;
 
     private bool isTitle;
 
@@ -26,16 +32,19 @@ public class StartSceneManager : MonoBehaviour
                 isTitle = true;
             }
         } else {
-
+            this.enabled = false;
         }
     }    
 
     public void StartGame(Stage selectStage){
         GameManager.instance.selectStage = selectStage;
-        SceneManager.LoadScene("01.InGameScene");
+
+        blackBackground.gameObject.SetActive(true);
+        Tween fadeTween = blackBackground.DOFade(1.0f, 0.75f);
+        fadeTween.OnComplete(() => SceneManager.LoadScene("01.InGameScene"));
     }
 
     private void OnDestroy() {
-        instance = null;    
+        instance = null;
     }
 }
