d51e56d24437839b92eaa25bcc7574a7b2bb689f
Komorio <twilivez@gmail.com>
2020-08-10 16:49:26 +0900

    [UPDATE] StarFall 안되던 문제 해결

--- Assets/01.Scripts/InGameManager/StarFall.cs
+++ Assets/01.Scripts/InGameManager/StarFall.cs
@@ -10,47 +10,48 @@ public class StarFall : MonoBehaviour
     private Image image;
 
     private float regenerateDuration;
-    
+    private IEnumerator timerCoroutine;
+
     private void Awake(){
         image = gameObject.GetComponent<Image>();
     }
     
     private void Start(){
         regenerateDuration = Random.Range(3.0f, 8.0f);
-        StarFallTimer().Start();
     }
 
     private Vector2 GetRandomPosition(){
         return Camera.main.ViewportToWorldPoint(new Vector3(Random.value, Random.value, 0));
     }
 
-    private IEnumerator StarFallTimer(){
-        float duration = regenerateDuration;
-        
-        while(true){
-            if(duration > 0){
-                duration -= Time.deltaTime;
-            } else {
-                regenerateDuration = Random.Range(3.0f, 8.0f);
-                duration = regenerateDuration;
-                gameObject.transform.position = GetRandomPosition();
-                yield return StarFallCoroutine().Start();
-            }
+    private void Update(){
+        if(regenerateDuration > 0){
+            regenerateDuration -= Time.deltaTime;
+        } else {
+            gameObject.transform.position = GetRandomPosition();
+            regenerateDuration = Random.Range(3.0f, 8.0f);
 
-            yield return YieldInstructionCache.WaitUntil;
+            StarFallCoroutine().Start();
         }
     }
 
     private IEnumerator StarFallCoroutine(){
-        float fillDuration = Random.Range(0.25f, 1.5f);
+        float fillDuration = Random.Range(0.75f, 1.5f);
         
-        float newScaleValue = Random.value;
+        float newScaleValue = Random.Range(1.5f, 2.5f);
         var newScaleVector = new Vector2(newScaleValue, newScaleValue);
         
         gameObject.transform.localScale = newScaleVector;
 
-        yield return image.DOFillAmount(1.0f, fillDuration);
-        yield return image.DOFillAmount(0.0f, 0.25f);
-    }
+        image.fillOrigin = 1;
 
+        var fillAmountTween = image.DOFillAmount(1.0f, fillDuration);
+        yield return fillAmountTween.WaitForCompletion();
+        
+        image.fillOrigin = 0;
+
+        fillAmountTween.Kill();
+        fillAmountTween = image.DOFillAmount(0.0f, 0.25f);
+        yield return fillAmountTween.WaitForCompletion();
+    }
 }
