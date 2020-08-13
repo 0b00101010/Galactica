a739526450f76e9b29e2a7fc2524122ea6f52054
Komorio <twilivez@gmail.com>
2020-08-13 10:38:15 +0900

    [ADD] StarFall Generator 추가

---
+++ Assets/01.Scripts/InGameManager/StarFallGenerator.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using Tempus.CoroutineTools;
+
+public class StarFallGenerator : MonoBehaviour
+{
+    private Queue<StarFall> starFallQueue = new Queue<StarFall>();
+
+    private void Awake(){
+        gameObject.GetComponentsInChildren<StarFall>(true).ToList().ForEach((item) => {
+            starFallQueue.Enqueue(item);
+        });
+    }
+
+    private void Start(){
+        StarFallGenerateCoroutine().Start();
+    }
+
+    private IEnumerator StarFallGenerateCoroutine(){
+        while(true){
+            yield return YieldInstructionCache.WaitingSeconds(0.25f);
+            GenerateStarFall();
+        }
+    }
+
+    public void GenerateStarFall(){
+        var starFall = starFallQueue.Dequeue();
+        starFall.Execute();
+        starFallQueue.Enqueue(starFall);
+    }
+}
